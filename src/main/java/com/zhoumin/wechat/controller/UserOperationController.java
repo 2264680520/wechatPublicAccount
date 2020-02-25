afd7c8152ebedf65d4db3065e69b1a46adfb674c
2264680520 <2264680520@qq.com>
2020-02-25 11:00:56 +0800

    登陆验证

--- src/main/java/com/zhoumin/wechat/controller/UserOperationController.java
+++ src/main/java/com/zhoumin/wechat/controller/UserOperationController.java
@@ -4,7 +4,10 @@ import com.zhoumin.wechat.dto.SuperAdmin;
 import com.zhoumin.wechat.result.ResponseEntity;
 import com.zhoumin.wechat.service.MessageService;
 import com.zhoumin.wechat.service.UserOperationService;
+import com.zhoumin.wechat.service.impl.MessageServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserOperationController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserOperationController.class);
+
     @Autowired
     UserOperationService userOperationService;
 
     @PostMapping("/login")
     public ResponseEntity login(String code) {
-        return userOperationService.userLogin(code);
+        ResponseEntity responseEntity=null;
+        try {
+            responseEntity = userOperationService.userLogin(code);
+        } catch (Exception e) {
+            LOGGER.info("获取用户信息失败");
+            return ResponseEntity.error();
+        }
+        return responseEntity;
     }
 }
